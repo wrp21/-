65f9455937e326433ea1c9e49a8030035c78d860
[2기-1반] 박우람 <ions789123@gmail.com>
2021-10-07 15:14:01 +0900

    add Loading Page

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FormContainer, LoadingMsg} from "../Styled/Styled";
+
+const Loading = ({history}) => {
+
+    return (
+        
+        <FormContainer className="Loading">
+            <label>
+                분석을 요청하고 있으니 잠시만 기다려주세요.
+            </label>
+        </FormContainer>
+        
+    );
+} 
+
+export default Loading;
