8a96911934ddc72a52c46d9293ec9c8ab3f4d8e7
Kimmie06 <kimms4579@gmail.com>
2021-10-10 14:22:13 +0900

    Mainpage Chart Viewing

---
+++ frontend/src/components/IntroChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Line} from 'react-chartjs-2';
+
+export const IntroChart = (props) => {
+
+    const list = props;
+
+    console.log(list.data[0]);
+
+}
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IntroChart } from "./IntroChart";
 
 const Main = ({history}) => {
     return (
@@ -6,7 +7,7 @@ const Main = ({history}) => {
             
             <p>예비 창업자를 위한 상권 분석 서비스는...... 입니다.</p>
             <button onClick={() => {history.push("/Survey")}}>분석하기</button>
-            
+            <IntroChart />
         </div>
     );
 
