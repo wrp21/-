40855670c888de5fcdf136b659306df00963e2b6
김서짱 <seojeong1101@gmail.com>
2021-10-12 11:08:03 +0900

    result page layout"

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -6,6 +6,15 @@ import Button from 'react-bootstrap/Button';
 import {Footer,MainContainer,Section} from '../Styled/Styled';
 import { BarChart } from './BarChart';
 
+
+
+
+// 스타일 관련
+import '../Styled/result.css';
+import { LineChart } from './LineChart';
+
+import Chart from 'chart.js/auto';
+
 const Result = ({history})=>{
 /////////////////////////////////////////////////////////////////////// location 사용
     const location = useLocation();
@@ -18,6 +27,11 @@ const Result = ({history})=>{
 
     const closeDate = Object.keys(data);
     const closeCount = Object.values(data);
+
+    useEffect(()=>{
+        //buildBarChart();
+        buildChart();
+    },[])
     
 
 
@@ -60,45 +74,109 @@ const Result = ({history})=>{
         document.documentElement.scrollTop = document.body.scrollHeight;
     };
 
+
+    // 라인 차트 그리기
+    const buildChart = () =>{
+        var ctx = document.getElementById("LineChart").getContext("2d");
+
+        //if(typeof LineChart !== 'undefined') LineChart.destroy();
+
+        let LineChart = new Chart(ctx,{
+            type:'line',
+            data:{
+                labels:closeDate,
+                datasets:[{
+                    label:'폐업 추이',
+                    data:closeCount,
+                    fill:false,
+                    borderColor:'rgb(75,192,192)',
+                    tension:0.1
+                }]
+            },
+        });
+    }
+
+    // 바 차트 그리기
+    
+    /*
+    const buildBarChart =()=>{
+        var ctx = document.getElementById("MyBarChart").getContext("2d");
+
+        let MyBarChart = new Chart(ctx,{
+            type:'bar',
+            data:{
+                labels:closeDate,
+                datasets:[{
+                    label:'폐업 추이',
+                    data:closeCount,
+                    fill:false,
+                    backgroundColor:'rgb(75,192,192)',
+                    tension:0.1
+                }]
+            },
+
+        });
+    }
+    */
+    
     return(
         <div>
-            <h1>결과 페이지</h1>
+            
+            <div id="MainContainer">
+                <div id="MainContainerHead">
+                    <h3><strong>{userSelect[1]}</strong> 지역 <strong>{userSelect[0]}</strong> 업종 폐업정보 </h3>
+                </div>
 
-            <MainContainer>
-                <Section>
-                    <article>
-                        
-                        <div>
-                            <h2>{userSelect[1]} 지역 {userSelect[0]} 업종 폐업정보 </h2>
-                            <BarChart data={[closeDate,closeCount]}></BarChart>
-                        </div>
-                        
-                        <p>본문</p>
-                    </article>
-                </Section>
-           
-                <Footer>    
+                <div id="GraphContainer">
+                    <p>코로나 이전</p>
+                    <BarChart data={[closeDate,closeCount]}></BarChart> 
+                </div>
+
+                <div id="GraphContainer">
+                    <p>코로나 이후</p>
+                    <BarChart data={[closeDate,closeCount]}></BarChart> 
+                </div>
+
+
+
+                <div id="MainContainerHead">
+                    <h3><strong>{userSelect[1]}</strong> 지역 <strong>{userSelect[0]}</strong> 업종 폐업 추이 </h3>
+                </div>
+
+                <div>
+                    <canvas id="LineChart" width="1000" height="400"/>
+                </div>
+
+                <div>
+                    <canvas id="MyBarChart"></canvas>
+                </div>
+
+                <footer>
                     <div>
+                        <Button onClick={toggleDiv1} style={{paddingRight:"10px", backgroundColor:'rgb(75,192,192)'}}>추천업종 보러가기</Button>
+                        <Button onClick={toggleDiv2} style={{paddingLeft:"10px",backgroundColor:'rgb(75,192,192)'}}>추천지역 보러가기</Button>
+
+                        <div id='recommend'>
+                            <div id='category'>
+                                <h3>{userSelect[1]} 추천 업종입니다.</h3>
+                                <BarChart data={[closeDate,closeCount]}></BarChart>
+                            
+                            </div>
+                            <div id='region'>
+                                <h3>{userSelect[0]} 추천 지역입니다.</h3>
+                                <BarChart data={[closeDate,closeCount]}></BarChart>
+                            
+                            </div>
+                        </div>
+
                         
-                        <Button onClick={toggleDiv1} style={{paddingRight:"10px"}}>추천업종 보러가기</Button>
-                        <Button onClick={toggleDiv2} style={{paddingLeft:"10px"}}>추천지역 보러가기</Button>
-
-                        <div id='category'>
-                            <h3>{userSelect[1]} 추천 업종입니다.</h3>
-                            <BarChart data={[closeDate,closeCount]}></BarChart>
-                            
-                        </div>
-                        <div id='region'>
-                            <h3>{userSelect[0]} 추천 지역입니다.</h3>
-                            <BarChart data={[closeDate,closeCount]}></BarChart>
-                            
-                        </div>
                     </div>
-                    
+                   
+                </footer>
 
-                </Footer>
-            </MainContainer>
+            </div>
         </div>
+            
     );
     
 }
--- frontend/src/components/Survey.js
+++ frontend/src/components/Survey.js
@@ -71,7 +71,7 @@ const Survey = ({history, location}) => {
 
         await axios({
             method: 'get',                                 // 수정 필요 => 'get'
-            url: 'http://172.30.1.41:5000/api/result',    // 수정 필요
+            url: 'http://172.30.1.2:5000/api/result',    // 수정 필요
             params: {region: regionRef.current.options[regionRef.current.selectedIndex].text, category: categoryRef.current.options[categoryRef.current.selectedIndex].text},
             headers: {'Content-Type': 'application/json'},
         })
